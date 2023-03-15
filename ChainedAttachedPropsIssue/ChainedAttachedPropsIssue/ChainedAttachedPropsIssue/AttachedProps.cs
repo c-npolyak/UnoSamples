0dafdc3a73fe089ae1e737f2b7ba51ac36ffbcc4
Nick Polyak <c-npolyak@kahua.com>
2023-03-15 13:25:44 -0400

    Reproduction of chained attached properies in Binding bug.

---
+++ ChainedAttachedPropsIssue/ChainedAttachedPropsIssue/ChainedAttachedPropsIssue/AttachedProps.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace ChainedAttachedPropsIssue
+{
+    public static class AttachedProps
+    {
+
+        #region MyElement attached Property
+        public static FrameworkElement GetMyElement(DependencyObject obj)
+        {
+            return (FrameworkElement)obj.GetValue(MyElementProperty);
+        }
+
+        public static void SetMyElement(DependencyObject obj, FrameworkElement value)
+        {
+            obj.SetValue(MyElementProperty, value);
+        }
+
+        public static readonly DependencyProperty MyElementProperty =
+        DependencyProperty.RegisterAttached
+        (
+            "MyElement",
+            typeof(FrameworkElement),
+            typeof(AttachedProps),
+            new PropertyMetadata(null)
+        );
+        #endregion MyElement attached Property
+
+
+        #region MyString attached Property
+        public static string GetMyString(DependencyObject obj)
+        {
+            return (string)obj.GetValue(MyStringProperty);
+        }
+
+        public static void SetMyString(DependencyObject obj, string value)
+        {
+            obj.SetValue(MyStringProperty, value);
+        }
+
+        public static readonly DependencyProperty MyStringProperty =
+        DependencyProperty.RegisterAttached
+        (
+            "MyString",
+            typeof(string),
+            typeof(AttachedProps),
+            new PropertyMetadata(null)
+        );
+        #endregion MyString attached Property
+
+    }
+}
